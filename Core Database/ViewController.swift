4fb1fd74fe63a87441790c5e46654ea0d403c16b
Dipika Choudhary <dipikachoudhary3104@gmail.com>
2023-04-06 12:11:43 +0530

    Initial Commit

---
+++ Core Database/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core Database
+//
+//  Created by Choudhary Dipika on 06/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
